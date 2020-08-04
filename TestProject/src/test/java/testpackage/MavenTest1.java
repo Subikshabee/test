7f8213d7e56ebd10997f509e29a8c8b88f6e79c4
Subikshabee <68991007+Subikshabee@users.noreply.github.com>
2020-08-04 20:52:41 +0530

    Update MavenTest1.java

--- TestProject/src/test/java/testpackage/MavenTest1.java
+++ TestProject/src/test/java/testpackage/MavenTest1.java
@@ -8,7 +8,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;  
 import org.testng.annotations.Test;  
 public class MavenTest1 {  
-public String baseUrl = "http://www.google.com/";  
+public String baseUrl1 = "http://www.google.com/";  
+public String baseUrl2 = "https://twingo.techmahindra.com/";  
 String driverPath = "IEDriverServer.exe";  
 public WebDriver driver ;   
 @Test             
@@ -19,7 +20,7 @@ System.setProperty("webdriver.ie.driver", driverPath);
 driver = new InternetExplorerDriver();  
 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);  
 driver.manage().window().maximize();  
-driver.get(baseUrl);  
+driver.get(baseUrl1);  
 // get the current URL of the page  
 String URL= driver.getCurrentUrl();  
 System.out.print(URL);  
@@ -29,6 +30,23 @@ String expectedtitle = "Google";
 Assert.assertEquals(title,expectedtitle);
 System.out.println(title);  
 }     
+public void twingo() {      
+	// set the system property for internet explorer driver      
+	System.setProperty("webdriver.ie.driver", driverPath);  
+	// Create driver object for CHROME browser  
+	driver = new InternetExplorerDriver();  
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);  
+	driver.manage().window().maximize();  
+	driver.get(baseUrl2);  
+	// get the current URL of the page  
+	String URL= driver.getCurrentUrl();  
+	System.out.print(URL);  
+	//get the title of the page  
+	String title = driver.getTitle(); 
+	String expectedtitle = "TWINGO";
+	Assert.assertEquals(title,expectedtitle);
+	System.out.println(title);  
+	}     
 @BeforeTest  
 public void beforeTest() {    
 System.out.println("before test");  
